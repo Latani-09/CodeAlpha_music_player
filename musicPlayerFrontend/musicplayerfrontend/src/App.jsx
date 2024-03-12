4a2fa92f179bbb341cfa0b8638e8baf9eab36b14
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2024-03-12 09:06:10 +0530

    Revert "playlist view improved with bootstrap and skip to prev & next songs"
    This reverts commit 13312da18c6e71666bdc6e1092992cb863749ffa.

--- musicPlayerFrontend/musicplayerfrontend/src/App.jsx
+++ musicPlayerFrontend/musicplayerfrontend/src/App.jsx
@@ -1,13 +1,13 @@
 
-import { useState, useEffect } from 'react';
+import { useState ,useEffect} from 'react';
+import './App.css';
 import * as data1 from './songsData.json';
 import Player from './component/player';
-import { Button, Dropdown } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { Button,Dropdown } from 'react-bootstrap';
 
 
 function App() {
+    const [selectedOption, setSelectedOption] = useState('Any');
 
     const [songs, setSongs] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -16,25 +16,18 @@ function App() {
     const [playlist, setplaylist] = useState(data1.songs);
     const [q, setQ] = useState('');
     const [searchResults, setSearchResults] = useState([]);
-    var selectedOption = null;   
-    const searchParam = ['title']; 
+
+    const searchParam = ['title']; // Add more parameters if needed
 
     useEffect(() => {
 
         const songs = data1.songs;
-        var i = 0;
-        songs.map((song, key) => {
-            song.id = i;
-            i++;
-        })
         console.log(songs); // output 'testing'
-
+        
         setplaylist(songs)
         setCurrentSong(songs[0]);
         setCurrentIndex(0);
     }, [songsnotFound]);
-
-                                            //function to filter by language
     const filterCombiner = (d, filterArray) => {
         for (let fn of filterArray) {
             if (!fn(d)) {
@@ -43,40 +36,52 @@ function App() {
         }
         return true;
     }
-
-    const filterByLanguage = async () => {
+    const filterByLanguage = () => {
         console.log('filter by', selectedOption);
-        if (selectedOption != 'Any Language') {
+        if (selectedOption != 'Any') {
             const filterArray1 = [
                 d => d.language === selectedOption,
             ];
             console.log(typeof (data1.songs), "type of songs")
-            const arr1 = data1.songs.filter(d => filterCombiner(d, filterArray1));   
-            var i = 0;
-            arr1.map((song, key) => {
-                song.id = i;
-                i++;
-            })
-
+            const arr1 = data1.songs.filter(d => filterCombiner(d, filterArray1));
             console.log('arr1', arr1);
             setplaylist(arr1);
-            console.log('playlist set to', playlist);
         }
-        else if (selectedOption == 'Any Language') {
+        else if (selectedOption=='Any'){
             setplaylist(data1.songs);
         }
     }
+    const nextSong = () => {
+        if (songsnotFound) {
+            setSongsFound(True);
+        }
+        if (currentIndex + 1 < songs.length) {
+            setCurrentIndex(currentIndex + 1)
+            setCurrentSong(songs[currentIndex + 1])
+        }
+    }
 
-                                                    //dropdown for Language filter
-    const options = ['Tamil', 'Hindi', 'English'];
+    const prevSong = () => {
+        if (songsnotFound) {
+            setSongsFound(True);
+        }
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1)
+            setCurrentSong(songs[currentIndex - 1])
+            console.log("current song",currentSong)
+        }
+    }
+  
+        // Example array of values
+        const options = ['Any','Tamil','Hindi', 'English'];
 
-    const handleDropdownChange = async (event) => {
-        console.log('filter by', event.target.value);
-        selectedOption = event.target.value;
-        await filterByLanguage();
-    };
+        
 
-                                                   //search component
+        const handleDropdownChange = (event) => {
+            setSelectedOption(event.target.value);
+        };
+
+
     const handleSearch = () => {
         const results = playlist.filter((song) =>
             song.title
@@ -84,59 +89,45 @@ function App() {
                 .toLowerCase()
                 .includes(q.toLowerCase())
         );
-        var i = 0;
-        results.map((song, key) => {
-            song.id = i;
-            i++;
-        })
+       
 
         setSearchResults(results);
         setplaylist(results);
 
     };
     return (
-        <div class="container"  >
-            {/* Search  & Language Filter*/ }
-            <nav class="navbar navbar-expand-lg navbar-dark " style={{ backgroundColor: 'hsl(240,60%,10%)', minHeight: "15vh", width: "100%" }}>
-                <div class="container-fluid" >
-                    <a class="navbar-brand" > Music Player</a>
+        
+        <div className="container">
+            <h3>Music Player</h3>
+            <div className="search-and-filter-wrapper">
+                <div className="filter-wrapper">
+                <label htmlFor="dropdown">Language:</label>
+                <select id="dropdown" value={selectedOption} onChange={handleDropdownChange}>
+                    {options.map((option, index) => (
+                        <option key={index} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+
+                    <Button onClick={filterByLanguage}>Filter</Button>
                 </div>
-                <div class="col-2 "  >
-                    <select class="form-select bg-dark " style={{ color: "white" }} aria-label="Default select example" id="dropdown" value={selectedOption} onChange={handleDropdownChange}>
-                        <option selected>Any Language</option>
-                        {options.map((option, index) => (
-                            <option key={index} value={option}>
-                                {option}
-                            </option>
-                        ))}
-                    </select>
+                <div className="search-wrapper">
+                    <input
+                        type="text"
+                        value={q}
+                        onChange={(e) => setQ(e.target.value)}
+                        placeholder="Search by title"
+                    />
+                    <Button onClick={handleSearch}>Search</Button>
                 </div>
-                <div class="col-3">
-                </div>
-                <div class="col-3 ">
-                    <div class="container-flex">
-                        <div class="row">
-                            <div class="col-6">
-                                <input class="form-control text-wrap"
-                                    style={{ borderRadius: "5px" }}
-                                    type="text"
-                                    value={q}
-                                    onChange={(e) => setQ(e.target.value)}
-                                    placeholder="Search by title"
-                                />
-                            </div>
-                            <div class="col-4 ">
-                                <Button variant="dark text-wrap" onClick={handleSearch}>Search</Button>
-                            </div>
-                            <div class="col-4">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-            
-            <Player playlist={playlist} /> {/*Play list and player component */}
+            </div>
+        <div className='player-main'>
+
+            <Player playlist={playlist} />
         </div>
+            
+       </div>
     );
 }
 
--- musicPlayerFrontend/musicplayerfrontend/src/component/player.jsx
+++ musicPlayerFrontend/musicplayerfrontend/src/component/player.jsx
@@ -1,55 +1,59 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { BsPauseCircleFill, BsSkipStartCircle, BsSkipEndCircle, BsPlay, BsPlayCircleFill, BsFillVolumeDownFill } from "react-icons/bs";
-import Stack from 'react-bootstrap/Stack';
-import { Row, Col, Container } from 'react-bootstrap';
+import React, { Component, audio, useRef ,useState} from 'react';
+import { useEffect } from 'react';
+                  
 export default function Player({
-    playlist,
-
-
+    playlist
 }) {
-    const [currentSong, setCurrent] = useState({ src: "", title: "", artist: [] });
-    const [currentIndex, setCurrentIndex] = useState("");
+    
+    useEffect(()=>{
+        console.log('player wroks', playlist)
+    },[]);
+    return (
+        
+                <table className="song-table ">
+                    <thead>
+                        <tr >
+                            <th class="col-5">Title</th>
+                            <th class="col-4">Artists</th>
+                            <th class="col-2">Language</th>
+                            <th class="col-1">Play</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {playlist.map(({ _id, fileName, title, language,artist }) => (
 
-    const [nextIndex, setNextIndex] = useState("");
-    const [paused, setpaused] = useState(true);
+                            <tr key={_id}>
+                                <td>{title}</td>
+                                <td>
+                                    {artist !== undefined ? (
+                                        <p>{artist.join(', ')}</p>
+                                    ) : (
+                                        <p></p>
+                                    )}
+                                </td>
+                                <td>{language}</td>
+                                <td>
+                                    <MediaComponent audioSrc={fileName} type="audio/mp3" />
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                </table>
+              
+           
+        );
+    }
+    
+
+
+const MediaComponent = ({audioSrc }) => {
+ 
+    let mp3Src = './songs/' + audioSrc;
+    console.log('mp3Src - ',mp3Src);
     const audioRef = useRef(null);
-    useEffect(() => {
-        console.log('player works', playlist)
-    }, []);
-    function findSongById(list, id) {
-        return list.find((obj) => obj._id === id);
-    }
-    const changeAudio = async (fileName,_id,id) => {
-
-        
-
-        // If the audioRef is not loaded, log a message
-        if (audioRef.current === null) {
-            console.log("Audio component is not loaded yet.");
-            return;
-        }
-
-        // Pause the current audio
-        audioRef.current.pause();
-        setpaused(true); // set pause state the audio
-        console.log('before changing', playlist[id])
-
-        // Set the new audio source  using id 
-        var songselected = playlist[id]
-        await setCurrent({ id: songselected.id, src: './songs/' + fileName, artist: songselected.artist, title: songselected.title });
-        console.log('_id', _id, 'artist', songselected.artist, 'title', songselected.title,playlist[id])
-        setCurrentIndex(id);
-        
-        await console.log('current song', currentSong.title);
-
-        // Play the new audio
-        await audioRef.current.play();
-        setpaused(false); // set pause state the audio
-
-    }
-
-
-
+    const [paused, setpaused] = useState(true);
+ 
     const changevolume = (e) => {
         audioRef.current.volume = e.target.value / 100;
     }
@@ -58,146 +62,26 @@ export default function Player({
         audioRef.current === null
             ? console.log("Audio component is not loaded yet.")
             : audioRef.current.paused
-                ?
-                audioRef.current.play()
+                ? 
+                    audioRef.current.play()
                 : audioRef.current.pause();
     }
-    const nextSong = () => {
 
-        console.log('nextSong');
-        console.log('index',currentIndex,'length',playlist.length)
-        if (currentIndex + 1 < playlist.length) {
-            setCurrentIndex(currentIndex + 1)
-            var song = playlist[currentIndex + 1]
-            setCurrent(song);
-            console.log(song)
-            changeAudio(song.fileName, song._id, song.id);
-        }
-    }
-
-    const prevSong = () => {
-
-        if (currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1)
-            var song = playlist[currentIndex - 1]
-            setCurrent(song);
-            console.log("current song", currentSong)
-            changeAudio(song.fileName, song._id, song.id);
-        }
-    }
-
+    
     return (
-        <div class="row">
-            <div class="row song-table-container" >
-                <table class="table table-dark song-table ">
-                    <thead class="bg-light sticky-top top-0">
-                        <tr >
-                            <th class="col-1"> </th>
-                            <th class="col-5">Title</th>
-                            <th class="col-4">Artists</th>
-                            <th class="col-2">Language</th>
+        <div >
 
-                        </tr>
-                    </thead>
-                    <tbody  >
-                    
-                        {playlist.map((song,key) => (
-
-                            <tr key={song._id} class="trow">
-                                <td class="col-1" >
-                                    <BsPlay
-                                        color='white'
-                                        className='icons'
-                                        size={30}
-                                        onClick={() => changeAudio(song.fileName, song._id,song.id)} />
-                                </td>
-                                <td class="col-5">{song.title}</td>
-                                <td class="col-4">
-                                    {song.artist !== undefined ? (
-                                        <p>{song.artist.join(', ')}</p>
-                                    ) : (
-                                        <p></p>
-                                    )}
-                                </td >
-                                <td class="col-2">{song.language}</td>
-
-                            </tr>
-                        ))}
-                        
-                    </tbody>
-                </table>
-
-            </div>
-
-            <div class="row   player-card" >
-                <div class="col-4 volume-bar">
-
-                    <audio ref={audioRef} src={currentSong.src} volume />
-                    <BsFillVolumeDownFill
-                        color={'white'}
-                        size={20} />
-                    <input
-                        type="range"
-                        defaultValue="40"
-                        className="mx-2 progressBarvolume bar volume"
-                        onChange={changevolume}
-                    />
-
-                </div>
+            <button onClick={toggleAudio} >
+                {paused ? ('play') : ('pause')}</button>
+            <audio ref={audioRef} src={mp3Src} volume/>
+            {paused ? (null) : (
+                <input
+                    type="range"
+                    defaultValue="40"
+                    className="mx-2 progressBarvolume bar volume"
+                    onChange={changevolume} />
                 
-                <div class="col-2">
-                <div class="row">
-                    <div class="col-3 ">
-                    <BsSkipStartCircle
-                        color='white'
-                        
-                        className='icons'
-                        size={40}
-                        onClick={prevSong}
-
-                        />
-                    </div>
-                    <div class="col-3 ">
-                    {paused ? (
-                        <BsPlayCircleFill
-                            color='white'
-                            className='icons'
-                            size={40}
-                                  
-                            onClick={toggleAudio}
-                        />
-                    ) : (
-                        <BsPauseCircleFill
-                            color='white'
-                            className='icons'
-                            size={40}
-                     
-                            onClick={toggleAudio}
-                        />
-                        )}
-                    </div>
-                    <div class="col-3 ">
-                    <BsSkipEndCircle
-                        color='white'
-                        size={40}
-                        className='icons'
-                        onClick={nextSong}
-                       
-                        /></div>
-                   
-                    </div></div>
-                <div class="col-5" style={{ color: "white" }}>
-                    <div class="row">
-
-                    <p >{currentSong.title.toLocaleUpperCase()}</p>
-                    <p>{currentSong.artist.join(', ')}</p>
-                </div>
-                </div>
-
-            </div>
+            )}
         </div>
-
     );
-}
-
-
+};
